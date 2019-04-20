fa058e98352e8b11763ce1bc525d2aa8b68e53a3
Andre Goh <andregoh1996@gmail.com>
2019-04-20 14:39:44 +0800

    Added names to search results

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const BusRouteTest = require("./models/BusRouteTest");
 const BusService = require("./models/busservice");
 const request = require("request");
 const cors = require("cors");
-
+const { Expo } = require("./node_modules/expo-server-sdk");
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
@@ -124,10 +124,10 @@ app.get("/api/search", (req, res) => {
         });
       BusStop.find(
         { BusStopCode: new RegExp(searchQ, "i") },
-        "BusStopCode",
+        "BusStopCode Description",
         (err, doc) => {
           doc.map(e => {
-            resData.push(e.BusStopCode);
+            resData.push(e.BusStopCode + " " + e.Description);
           });
           res.json(
             resData.sort((e1, e2) => {
@@ -153,5 +153,56 @@ app.get("/api/routes", (req, res) => {
       }
     }
   ).sort({ StopSequence: 1 });
+});
+
+app.post("/users/pushtoken", (req, res) => {
+  // Create a new Expo SDK client
+  let expo = new Expo();
+
+  // Create the messages that you want to send to clents
+  let messages = [];
+  for (let pushToken of somePushTokens) {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+    messages.push({
+      to: pushToken,
+      sound: "default",
+      body: "This is a test notification",
+      data: { withSome: "data" }
+    });
+  }
+
+  // The Expo push notification service accepts batches of notifications so
+  // that you don't need to send 1000 requests to send 1000 notifications. We
+  // recommend you batch your notifications to reduce the number of requests
+  // and to compress them (notifications with similar content will get
+  // compressed).
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
 });
 app.listen(port, () => console.log(`Server running on port ${port}`));
